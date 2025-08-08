b6e20442c8362d831ace1c888dd325b7b69dfb87
ashwinsayenju <105905011+asayenju@users.noreply.github.com>
2025-08-08 16:47:28 +0545

    Array mutation

---
+++ array_mutation.py
@@ -0,0 +1,31 @@
+"""
+Array Mutation
+Given an integer n and an array a of length n, your task is to apply the following mutation to a:
+
+Array a mutates into a new array b of length n.
+For each i from 0 to n - 1, b[i] = a[i - 1] + a[i] + a[i + 1].
+If some element in the sum a[i - 1] + a[i] + a[i + 1] does not exist, it should be set to 0. For example, b[0] should be equal to 0 + a[0] + a[1].
+Example
+
+For n = 5 and a = [4, 0, 1, -2, 3], the output should be solution(n, a) = [4, 5, -1, 2, 1].
+
+b[0] = 0 + a[0] + a[1] = 0 + 4 + 0 = 4
+b[1] = a[0] + a[1] + a[2] = 4 + 0 + 1 = 5
+b[2] = a[1] + a[2] + a[3] = 0 + 1 + (-2) = -1
+b[3] = a[2] + a[3] + a[4] = 1 + (-2) + 3 = 2
+b[4] = a[3] + a[4] + 0 = (-2) + 3 + 0 = 1
+So, the resulting array after the mutation will be [4, 5, -1, 2, 1].
+"""
+def mutation(array):
+    a = [0]*len(array)
+    for i in range(len(array)):
+        if i == 0:
+            a[i] += array[i]+a[i+1]
+        elif i == len(array)-1:
+            a[i] += array[i-1]+array[i]
+        else:
+            a[i] += array[i-1]+array[i]+array[i+1]
+    return a
+
+print(mutation([4, 0, 1, -2, 3]))
+
